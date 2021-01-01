8e11749e24f910b06be4a9f21d4a6042ba63103e
Kamata Shoichi <nikaidosho@gmail.com>
2021-01-01 02:19:28 +0000

    mynews2

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,15 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'],function(){
     Route::get('news/create','Admin\NewsController@add');
 });
+
+Route::group(['prefix' => 'XXX'],function(){
+    Route::get('AAA/create','XXX\AAAController@bbb');
+});
+
+Route::group(['prefix' => 'admin'],function(){
+    Route::get('profile/create','Admin\ProfileController@add');
+});
+
+Route::group(['prefix' => 'admin'],function(){
+    Route::get('profile/edit','Admin\ProfileController@edit');
+});
