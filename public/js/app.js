59d9adaa8a33ac53382c967c1bf7cbd03d6895ca
Kamata Shoichi <nikaidosho@gmail.com>
2021-01-04 02:13:47 +0000

    kadai

--- public/js/app.js
+++ public/js/app.js
@@ -49875,7 +49875,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\n\n    color: #fff;\n        ^\n      Expected \";\".\n  ╷\n5 │     color: #fff;\n  │          ^\n  ╵\n  stdin 5:10  root stylesheet\n      in /home/ec2-user/environment/mynews/resources/sass/admin.scss (line 5, column 10)\n    at runLoaders (/home/ec2-user/environment/mynews/node_modules/webpack/lib/NormalModule.js:316:20)\n    at /home/ec2-user/environment/mynews/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /home/ec2-user/environment/mynews/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/home/ec2-user/environment/mynews/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at render (/home/ec2-user/environment/mynews/node_modules/sass-loader/dist/index.js:89:7)\n    at Function.call$2 (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:90950:16)\n    at _render_closure1.call$2 (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:79870:12)\n    at _RootZone.runBinary$3$3 (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:27175:18)\n    at _FutureListener.handleError$1 (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:25703:19)\n    at _Future__propagateToListeners_handleError.call$0 (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:26000:49)\n    at Object._Future__propagateToListeners (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:4539:77)\n    at _Future._completeError$2 (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:25833:9)\n    at _AsyncAwaitCompleter.completeError$2 (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:25176:12)\n    at Object._asyncRethrow (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:4288:17)\n    at /home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:13184:20\n    at _wrapJsFunctionForAsync_closure.$protected (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:4313:15)\n    at _wrapJsFunctionForAsync_closure.call$2 (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:25197:12)\n    at _awaitOnObject_closure0.call$2 (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:25189:25)\n    at _RootZone.runBinary$3$3 (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:27175:18)\n    at _FutureListener.handleError$1 (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:25703:19)\n    at _Future__propagateToListeners_handleError.call$0 (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:26000:49)\n    at Object._Future__propagateToListeners (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:4539:77)\n    at _Future._completeError$2 (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:25833:9)\n    at _Future__asyncCompleteError_closure.call$0 (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:25922:18)\n    at Object._microtaskLoop (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:4590:24)\n    at StaticClosure._startMicrotaskLoop (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:4596:11)\n    at _AsyncRun__scheduleImmediateJsOverride_internalCallback.call$0 (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:25087:21)\n    at invokeClosure (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:1397:26)\n    at Immediate._onImmediate (/home/ec2-user/environment/mynews/node_modules/sass/sass.dart.js:1418:18)\n    at runCallback (timers.js:789:20)\n    at tryOnImmediate (timers.js:751:5)\n    at processImmediate [as _immediateCallback] (timers.js:722:5)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
@@ -49890,16 +49890,28 @@ throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\
 
 /***/ }),
 
+/***/ "./resources/sass/profile.scss":
+/*!*************************************!*\
+  !*** ./resources/sass/profile.scss ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*****************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/admin.scss ***!
-  \*****************************************************************************************/
+/*!***********************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/admin.scss ./resources/sass/profile.scss ***!
+  \***********************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /home/ec2-user/environment/mynews/resources/js/app.js */"./resources/js/app.js");
 __webpack_require__(/*! /home/ec2-user/environment/mynews/resources/sass/app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! /home/ec2-user/environment/mynews/resources/sass/admin.scss */"./resources/sass/admin.scss");
+__webpack_require__(/*! /home/ec2-user/environment/mynews/resources/sass/admin.scss */"./resources/sass/admin.scss");
+module.exports = __webpack_require__(/*! /home/ec2-user/environment/mynews/resources/sass/profile.scss */"./resources/sass/profile.scss");
 
 
 /***/ })
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -11,3 +11,17 @@
         <h1>MyProfile作成画面</h1>
     </body>
 </html>
+
+@extends('layouts.admin')
+
+@section('title','プロフィールの作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
