39284162cec0863ff0cdfdad0911f1ba7307aeb1
Kamata Shoichi <nikaidosho@gmail.com>
2021-01-09 06:55:53 +0000

    13

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,14 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'],function(){
     Route::post('news/create','Admin\NewsController@create');
 });
 
-Route::group(['prefix' => 'admin'],function(){
-    Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'],function(){
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::post('profile/create','Admin\ProfileController@create');
 });
 
-Route::group(['prefix' => 'admin'],function(){
-    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'],function(){
+    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::post('profile/edit','Admin\ProfileController@update');
 });
 
 Auth::routes();
