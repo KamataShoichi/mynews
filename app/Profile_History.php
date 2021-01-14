3d621393475b23b4f6383fa08c7dbe65ab67e3be
Kamata Shoichi <nikaidosho@gmail.com>
2021-01-14 09:03:58 +0000

    17

--- app/Profile_History.php
+++ app/Profile_History.php
@@ -9,7 +9,7 @@ class Profile_History extends Model
     protected $guarded = array('id');
     
     public static $rules = array(
-        profile_id => 'required',
-        edited_at => 'required',
+        'profile_id' => 'required',
+        'edited_at' => 'required',
         );
 }
